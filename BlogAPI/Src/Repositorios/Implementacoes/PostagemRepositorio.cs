abca7463f695b7dcf642086a458b4b3d7b91b6a7
thaina-barbosa <thaina.mbarbosaa@gmail.com>
2022-08-15 11:02:25 -0300

    Adicionando Projeto

--- BlogAPI/Src/Repositorios/Implementacoes/PostagemRepositorio.cs
+++ BlogAPI/Src/Repositorios/Implementacoes/PostagemRepositorio.cs
@@ -18,46 +18,69 @@ namespace BlogAPI.Src.Repositorios.Implementacoes
         {
             _contexto = contexto;
         }
-        #endregion Construtores
 
-        #region Métodos
-        public async Task<List<Postagem>> PegarTodasPostagensAsync()
+        public async Task AtualizarPostagemAsync(Postagem postagem)
         {
-            return await _contexto.Postagens
-            .Include(p => p.Criador)
-            .Include(p => p.Tema)
-            .ToListAsync();
+            if (!ExisteTemaId(postagem.Tema.Id)) throw new Exception("Id do tema não encontrado");
+            
+            var postagemExistente = await PegarPostagemPeloIdAsync(postagem.Id);
+            postagemExistente.Titulo = postagem.Titulo;
+            postagemExistente.Descricao = postagem.Descricao;
+            postagemExistente.Foto = postagem.Foto;
+            postagemExistente.Tema = await _contexto.Temas.FirstOrDefaultAsync(t => t.Id== postagem.Tema.Id);
+            
+            _contexto.Postagens.Update(postagemExistente);
+            await _contexto.SaveChangesAsync();
+             
+            // funções auxiliares
+            bool ExisteTemaId(int id)
+            {
+                var auxiliar = _contexto.Temas.FirstOrDefault(t => t.Id == id);
+                return auxiliar != null;
+            }
         }
 
-        public async Task<Postagem> PegarPostagemPeloIdAsync(int id)
+        private async Task<Postagem> PegarPostagemPeloIdAsync(int id)
         {
             if (!ExisteId(id)) throw new Exception("Id da postagem não encontrado");
+           
             return await _contexto.Postagens
             .Include(p => p.Criador)
             .Include(p => p.Tema)
             .FirstOrDefaultAsync(p => p.Id == id);
+           
             bool ExisteId(int id)
             {
                 var auxiliar = _contexto.Postagens.FirstOrDefault(u => u.Id == id);
                 return auxiliar != null;
             }
         }
+
+
+        public async Task DeletarPostagemAsync(int id)
+        {
+            _contexto.Postagens.Remove(await PegarPostagemPeloIdAsync(id));
+            await _contexto.SaveChangesAsync();
+        }
+
+
         public async Task NovaPostagemAsync(Postagem postagem)
         {
             if (!ExisteUsuarioId(postagem.Criador.Id)) throw new Exception("Id do usuário não encontrado");
 
             if (!ExisteTemaId(postagem.Tema.Id)) throw new Exception("Id do tema não encontrado");
-
+            
             await _contexto.Postagens.AddAsync(
                 new Postagem
-                {
-                     Titulo = postagem.Titulo,
-                     Descricao = postagem.Descricao,
-                     Foto = postagem.Foto,
-                     Criador = await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Id == postagem.Criador.Id),
+                 {
+                    Titulo = postagem.Titulo,
+                    Descricao = postagem.Descricao,
+                    Foto = postagem.Foto,
+                    Criador = await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Id == postagem.Criador.Id),
                     Tema = await _contexto.Temas.FirstOrDefaultAsync(t => t.Id == postagem.Tema.Id)
                 });
             await _contexto.SaveChangesAsync();
+            
             // funções auxiliares
             bool ExisteUsuarioId(int id)
             {
@@ -71,37 +94,25 @@ namespace BlogAPI.Src.Repositorios.Implementacoes
             }
         }
 
-        public async Task AtualizarPostagemAsync(Postagem postagem)
+        public async Task<List<Postagem>> PegarTodasPostagensAsync()
         {
-            if (!ExisteTemaId(postagem.Tema.Id)) throw new Exception("Id do tema não encontrado");
-
-            Postagem postagemExistente = await PegarPostagemPeloIdAsync(postagem.Id);
-            postagemExistente.Titulo = postagem.Titulo;
-            postagemExistente.Descricao = postagem.Descricao;
-            postagemExistente.Foto = postagem.Foto;
-            postagemExistente.Tema = await _contexto.Temas.FirstOrDefaultAsync(t => t.Id == postagem.Tema.Id); 
-            
-            _contexto.Postagens.Update(postagemExistente);  await _contexto.SaveChangesAsync();
-            
-            // funções auxiliares
-            bool ExisteTemaId(int id)
-            {
-                var auxiliar = _contexto.Temas.FirstOrDefault(t => t.Id == id);
-                return auxiliar != null;
-            }
+            return await _contexto.Postagens
+            .Include(p => p.Criador)
+            .Include(p => p.Tema)
+            .ToListAsync();
         }
 
-        public async Task DeletarPostagemAsync(int id)
+        Task<Postagem> IPostagem.PegarPostagemPeloIdAsync(int id)
         {
-            _contexto.Postagens.Remove(await PegarPostagemPeloIdAsync(id));
-            await _contexto.SaveChangesAsync();
+            throw new NotImplementedException();
         }
 
-
-
-
+        #endregion Construtores
+        #region Métodos
         #endregion Métodos
-
-
     }
 }
+
+
+
+
